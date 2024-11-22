bd285da5866f4b7f3c602d5fd02a90cf8220244b
Merah-create <newmera03@gmail.com>
2024-11-21 22:35:17 -0400

    Add files via upload

---
+++ style/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
